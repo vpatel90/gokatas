e15d14c52023c32a57e81a2f059e913084345549
Vivek Patel <vnp229@gmail.com>
2016-05-22 13:27:45 -0500

    added func change to actually calculate change for amount

--- coinchanger.go
+++ coinchanger.go
@@ -4,15 +4,27 @@ import "fmt"
 
 func main() {
 	n := 4
-	fmt.Println("Coins :", coinChanger(n))
+	fmt.Println("Coins [1 1 1 1]:", coinChanger(n))
 }
 
 func coinChanger(amount int) []int{
+	values := []int{25, 10, 5 , 1}
 	coins := make([]int, 0)
-	fmt.Println(amount)
-	for i := 1; i <= amount; i++ {
-		coins = append(coins, 1)
 
+	for _,element := range values {
+		remaining, sumValue := change(amount, element)
+		amount = remaining
+		
+		for i := 1; i <= sumValue; i++ {
+			coins = append(coins, element)
+		}
 	}
+
 	return coins
+}
+
+func change(amount, value int) (remaining, sumValue int) {
+	sumValue = amount / value // gets total coins of a value used
+	remaining = amount % value //gets new total after total coins used
+	return remaining, amount / value
 }
